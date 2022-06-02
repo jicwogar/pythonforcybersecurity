4985fad8a82e962bded1eb5c5e604812b7a9e2ea
jicwogar <jicwogar@yahoo.com>
2022-06-02 12:45:48 -0400

    modified my ping script

--- start/CH02/mypingscript.py
+++ start/CH02/mypingscript.py
@@ -8,9 +8,9 @@ ip = "127.0.0.1"
 current_os = platform.system().lower()
 #craft ping command according to os
 if current_os =="windows":
-    ping_cmd = "fping -n 1 -w 2 {ip} > null"
+    ping_cmd = f"ping -n 1 -w 2 {ip} > nul"
 else:
-    ping_cmd = "fping -c -1 -w {ip} >/dev/null 2>&1"
+    ping_cmd = f"ping -c 1 -w 2 {ip} >/dev/null 2>&1"
 exit_code = os.system(ping_cmd)
 print(exit_code)
     
