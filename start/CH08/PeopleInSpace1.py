9c5b701b853998db03f080538a878f7b8383121c
jicwogar <jicwogar@yahoo.com>
2022-06-21 04:05:17 -0400

    api request script

--- start/CH08/PeopleInSpace1.py
+++ start/CH08/PeopleInSpace1.py
@@ -1,1 +1,11 @@
-# paste code here
+import requests
+
+url = "http://api.open-notify.org/astros.json"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
